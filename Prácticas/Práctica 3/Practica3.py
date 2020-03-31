f29393b82356ed6dd39468f40f5efc5e3226a2cb
TheAeryan <carlosmarista@gmail.com>
2020-03-31 13:30:41 +0200

    Descargado guión prácticas

--- Prácticas/Práctica 3/Practica3.py
+++ Prácticas/Práctica 3/Practica3.py
@@ -1,22 +1,50 @@
-# -*- coding: utf-8 -*-
 
-import math
 
-# Calcula la raíz cuadrada de un número bien calculada, usada el método
-# de Newton Raphson
+#Calcula el máximo común divisor de dos números.
+def mcd(a,b):
+    while b!=0:
+        (a,b) = (b,a%b)
+    return a
+
+#Calcula el inverso de a módulo b (si existe). Si ni existe, lo dice y devuelve 0.
+def inversomodular(a,b):
+    (u0,u1) = (1,0)
+    while b>0:
+       (u0,u1) = (u1,u0-(a//b)*u1)
+       (a,b) = (b,a%b)
+    if a == 1:
+       return u0
+    else:
+       print("No existe el inverso")
+       return 0
+
+#Resuelve la congruencia ax = b mod m. Da todas las soluciones comprendidas entre 0 y m-1.
+def congruencia(a,b,m):
+    d = mcd(a,m)
+    if b%d == 0:
+       n = m//d
+       u = inversomodular(a//d,n)
+       x = (u*(b//d))%n
+       sol = []
+       for i in range(d):
+           sol.append(x)
+           x += n
+       return sol
+    print("La congruencia no tiene solución")
+    return([0])
+
+#Calcula la raíz cuadrada entera de un número natural.
 def raiz(n):
-    i = 0
     m = (len(bin(n))-1) // 2
-    x = 1 << m
+    x = 1<<n #x=2^m
     y = (x**2+n)//(2*x)
     while x > y:
         (x,y) = (y,(y**2+n)//(2*y))
-        i+=1
-    return [x,i] # i -> número de iteraciones necesarias para calcular sqrt(n)
+    return x
 
+#Comprueba si un número natural es cuadrado perfecto.
+def escuadrado(n):
+    y = raiz(n)
+    return(y**2 == n)
 
-a = 1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
-
-print('%F' % math.sqrt(a*a+a))
-print(raiz(a*a+a))
 
