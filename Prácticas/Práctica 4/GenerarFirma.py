ad4934f45f1745098d482eeeb23900b3f66c8975
GabrielaAntolinez <36423718+GabrielaAntolinez@users.noreply.github.com>
2020-06-10 20:20:43 +0200

    Update GenerarFirma.py
    Generación de Firma

--- Prácticas/Práctica 4/GenerarFirma.py
+++ Prácticas/Práctica 4/GenerarFirma.py
@@ -8,4 +8,27 @@ Práctica 4 de Criptografía - Firma Digital
 @author: Alessandro Zito
 """
 
-# ¡Usar función resumen (h(m)) SHA-2 si es posible! (usar alguna librería)
+import hashlib as h
+from random import randint
+from GenerarClaves import *
+from Practica1 import *
+
+def generacacion_firma(m):
+    #Se hace el resumen del mensaje utilizando SHA-2.
+    z = h.sha256(m)
+    #Se genera un número aleatorio de 2 a q-2.
+    k = randint(2,q-2)
+    #Se calcula la primera parte del par de la firma.
+    r = potencia_modular(alfa, k, p) % q
+    #Se calcula la segunda parte del par de la firma.
+    s = ((k ** (-1)) * (z+xr)) % q
+    #Si r o s fuera igual a 0 se recalcula la firma.
+    if (r==0 or s==0):
+        generacacion_firma(m)
+    else:
+        nom_fich_firm = "firma.txt"
+        with open(nom_fich_firm, 'w') as fich_firm:
+            fich_firm.write(str(r) + '\n')
+            fich_firm.write(str(s) + '\n')
+        return r,s
+    
